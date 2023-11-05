ba529811296447c6b3bd2fc536ce3e6c6f9cf247
achref douss <achrefdouss@gmail.com>
2023-11-05 17:30:43 +0100

    angular front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoApp';
+
+  readonly APIUrl="http://localhost:5116/api/todoapp/"
+constructor(private http:HttpClient)
+{
+
 }
+  notes:any=[];
+refreshNotes(){
+  this.http.get(this.APIUrl+'GetNotes').subscribe(data=>{
+    this.notes = data;
+  })
+}
+ngOnInit(){
+  this.refreshNotes();
+}
+addNotes(){
+  var newNotes=(<HTMLInputElement>document.getElementById("newNotes")).value;
+  var formData=new FormData();
+  formData.append("newNotes",newNotes);
+  this.http.post(this.APIUrl+'AddNotes',formData).subscribe(data=>{
+    alert(data);
+    this.refreshNotes();
+  })
+}
+
+deleteNotes(id:any){
+  this.http.delete(this.APIUrl+'DeleteNotes?id='+id).subscribe(data=>{
+    alert(data);
+    this.refreshNotes();
+  })
+}
+
+}
+
